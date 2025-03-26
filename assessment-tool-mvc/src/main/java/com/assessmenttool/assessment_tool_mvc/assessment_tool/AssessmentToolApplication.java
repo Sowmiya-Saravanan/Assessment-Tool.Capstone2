e7c73d7289d97d4aecf7b8b950a4b3d9a88023f8
Darsini-Chandrababu <darsinichandrababucm@gmail.com>
2025-03-26 11:29:54 +0530

    assessment-tool-mvc initial commit

---
+++ assessment-tool-mvc/src/main/java/com/assessmenttool/assessment_tool_mvc/assessment_tool/AssessmentToolApplication.java
@@ -0,0 +1,13 @@
+package com.assessmenttool.assessment_tool_mvc.assessment_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentToolApplication.class, args);
+	}
+
+}
---
+++ assessment-tool-mvc/src/test/java/com/assessmenttool/assessment_tool_mvc/assessment_tool/AssessmentToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessmenttool.assessment_tool_mvc.assessment_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
